2a193f44e6906a6fb73d257ec373498a3244305c
Bangnguyen2206 <tuyetbang0304@gmail.com>
2023-06-09 12:46:38 +0700

    Change opacity of button

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { useLayoutEffect } from "react";
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HeroImage } from "../assets";
+import * as Animatable from "react-native-animatable";
 
 function HomeScreen() {
   const navigation = useNavigation();
@@ -37,7 +38,22 @@ function HomeScreen() {
       <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36"></View>
       {/* Image container */}
       <View className="flex-1 relative items-center justify-center">
-        <Image source={HeroImage} className="w-full h-full object-cover" />
+        <Animatable.Image
+          animation="fadeIn"
+          easing="ease-in-out"
+          source={HeroImage}
+          className="w-full h-full object-cover mt-20"
+        />
+        <TouchableOpacity className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9] rounded-full items-center justify-center">
+          <Animatable.View
+            animation={"pulse"}
+            easing="ease-in-out"
+            iterationCount={"infinite"}
+            className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]"
+          >
+            <Text className="text-gray-50 text-[36px] font-semibold">Go</Text>
+          </Animatable.View>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
