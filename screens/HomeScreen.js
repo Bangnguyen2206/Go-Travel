8f38350a09401a66200e0d75615f51fffd3ec939
Bangnguyen2206 <tuyetbang0304@gmail.com>
2023-07-19 21:30:38 +0700

    udpate validation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { BgImage } from "../assets";
 import * as Animatable from "react-native-animatable";
+import * as Yup from "yup";
 import { CheckBox } from "react-native-elements";
 import { Button } from "@rneui/themed";
+import { useFormik } from "formik";
+import { Formik } from "formik";
 import {
   View,
   Text,
@@ -20,6 +23,7 @@ const styles = StyleSheet.create({
     height: 40,
     borderWidth: 1,
     padding: 10,
+    marginVertical: 10,
   },
   checkboxContainer: {
     flexDirection: "row",
@@ -40,9 +44,14 @@ function HomeScreen() {
       headerShown: false,
     });
   }, []);
+  const [initialState, setInitialState] = useState({
+    firstName: "",
+    lastName: "",
+    password: "",
+    email: "",
+  });
   return (
     <SafeAreaView className="bg-white flex-1 relative">
-      {/* Image container */}
       <View>
         <Animatable.Image
           animation="fadeIn"
@@ -51,88 +60,117 @@ function HomeScreen() {
           className="w-full h-[180px] object-cover"
         />
       </View>
-      <View className="mx-5 mt-3">
+      <View className="mx-5 mt-10">
         <Text className="text-lg">Adventure starts here</Text>
         <Text className="text-base font-light">
           Make your app management easy and fun
         </Text>
-        <View className="my-2">
-          <Text
-            for="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            First name
-          </Text>
-          <TextInput
-            style={styles.input}
-            // onChangeText={onChangeNumber}
-            value={"123"}
-            placeholder="useless placeholder"
-            keyboardType="numeric"
-          />
-        </View>
-        <View className="my-2">
-          <Text
-            for="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Last name
-          </Text>
-          <TextInput
-            style={styles.input}
-            value={"123"}
-            placeholder="useless placeholder"
-            keyboardType="numeric"
-          />
-        </View>
-        <View className="my-2">
-          <Text
-            for="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Email
-          </Text>
-          <TextInput
-            style={styles.input}
-            // onChangeText={onChangeNumber}
-            value={"123"}
-            placeholder="useless placeholder"
-            keyboardType="numeric"
-          />
-        </View>
-        <View className="my-2">
-          <Text
-            for="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
-            Password
-          </Text>
-          <TextInput
-            style={styles.input}
-            // onChangeText={onChangeNumber}
-            value={"123"}
-            placeholder="useless placeholder"
-            keyboardType="numeric"
-            secureTextEntry={true}
-          />
-        </View>
-        <View className="mt-2 flex-1 justify-start items-center flex-row">
-          <CheckBox
-            title="I agree to privacy policy & terms"
-            checked={true}
-            containerStyle={{
-              margin: 0,
-              marginLeft: 0,
-              marginRight: 0,
-              backgroundColor: "transparent",
-              border: "none",
-            }}
-            // onPress={() => setCheck1(!check1)}
-          />
-        </View>
-        <View className="mt-7">
-          <Button color="purple">Secondary</Button>
-        </View>
+
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            password: "",
+            email: "",
+          }}
+          validationSchema={Yup.object({
+            firstName: Yup.string().required("Firstname is not empty"),
+            lastName: Yup.string().required("Lastname is not empty"),
+            email: Yup.string().required("Email is not empty"),
+          })}
+          onSubmit={async (values) => {
+            console.log(values);
+          }}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <View className="my-2">
+              <Text className="block text-lg font-medium text-gray-500 dark:text-white">
+                First name <Text className="text-red-600">*</Text>
+              </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange("firstName")}
+                onBlur={handleBlur("firstName")}
+                value={values.firstName}
+                placeholder="Enter your first name..."
+              />
+              {touched.firstName && errors.firstName && (
+                <Text
+                  style={{ fontSize: 12, color: "#FF0D10" }}
+                  className="mb-2"
+                >
+                  {errors.firstName}
+                </Text>
+              )}
+              <Text className="block text-lg font-medium text-gray-500 dark:text-white">
+                Last name <Text className="text-red-600">*</Text>
+              </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange("lastName")}
+                onBlur={handleBlur("lastName")}
+                value={values.lastName}
+                placeholder="Enter your last name..."
+              />
+              {touched.lastName && errors.lastName && (
+                <Text
+                  style={{ fontSize: 12, color: "#FF0D10" }}
+                  className="mb-2"
+                >
+                  {errors.lastName}
+                </Text>
+              )}
+              <Text className="block text-lg font-medium text-gray-500 dark:text-white">
+                Email <Text className="text-red-600">*</Text>
+              </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange("email")}
+                onBlur={handleBlur("email")}
+                value={values.email}
+                placeholder="Enter your email..."
+              />
+              {touched.email && errors.email && (
+                <Text
+                  style={{ fontSize: 12, color: "#FF0D10" }}
+                  className="mb-2"
+                >
+                  {errors.email}
+                </Text>
+              )}
+              <Text className="block text-lg font-medium text-gray-500 dark:text-white">
+                Password <Text className="text-red-600">*</Text>
+              </Text>
+              <TextInput
+                style={styles.input}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                placeholder="Enter your password..."
+                secureTextEntry={true}
+              />
+              {touched.password && errors.password && (
+                <Text
+                  style={{ fontSize: 12, color: "#FF0D10" }}
+                  className="mb-2"
+                >
+                  {errors.password}
+                </Text>
+              )}
+              <View className="mt-7">
+                <Button onPress={handleSubmit} title="Sign Up" color="purple" />
+              </View>
+            </View>
+          )}
+        </Formik>
+
         <View className="mt-3 my-5 flex justify-center items-center flex-row">
           <Text className="block mb-2 text-lg font-medium text-gray-900 dark:text-white">
             Already have an account?
