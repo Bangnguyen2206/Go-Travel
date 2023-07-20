4f4ccdf2070f2fd8bc2bee254ea50f1c7dc414b8
Bangnguyen2206 <tuyetbang0304@gmail.com>
2023-07-20 09:46:09 +0700

    Modify package

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { TailwindProvider } from "tailwindcss-react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Toast from "react-native-toast-message";
+
 import Register from "./screens/Register";
 import LogIn from "./screens/Login";
-import Toast from "react-native-toast-message";
 
 const Stack = createNativeStackNavigator();
 
